01ea46c18b6389b34452cdff3cc57c81854f6c73
Abubakarikashaf <110847996+Abubakarikashaf@users.noreply.github.com>
2022-09-01 15:02:02 +0000

    commit 1

---
+++ results.js
@@ -0,0 +1,5 @@
+const results = [{name:"comfort yelfaare", class: "form three", grade: "8"},
+                 {name:"Atif Adamu", class: "final year", grade:"9"},
+                 {name:"abubakari kashaf", class: "final year", grade:"6"}
+]
+module.exports=results
